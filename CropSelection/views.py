0a4d9dec0a7c41a01377b508786d778481bab046
오수현 <elena0315@naver.com>
2024-08-20 18:35:31 +0900

    Fix: send false data for testing

--- CropSelection/views.py
+++ CropSelection/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+import random
+
 import jwt
 from rest_framework import status
 from rest_framework.views import APIView
@@ -104,10 +107,27 @@ class CropEnvironment(APIView):
             cnt += 1
             if cnt == 5: break
 
-        print(defEnv)
         optEnvSerializer = OptEnvSerializer(defEnv)
 
-        response = Response({"Current": currEnvSerializers,
+        falseSerializers = []
+        start = datetime.now() - timedelta(hours=10)
+        interval = timedelta(hours=2)
+        for i in range(5):
+            start += interval
+            # rand = random.uniform(0, 5)
+
+            falseData = {
+                "farmID": farmID,
+                "timestamp": start,
+                "N": 79.89+random.uniform(-5, 5), "P": 47.58+random.uniform(0, 5), "K": 39.87+random.uniform(0, 5),
+                "temperature": 23.689332+random.uniform(0, 5),
+                "humidity": 82.272822+random.uniform(0, 5),
+                "ph": 6.425471+random.uniform(0, 5),
+                "rainfall": 236.181114+random.uniform(0, 5)
+            }
+            falseSerializers.append(falseData)
+
+        response = Response({"Current": falseSerializers, # currEnvSerializers,
                              "Opt": optEnvSerializer.data}, status.HTTP_200_OK)
         response['Authorization'] = 'Bearer ' + auth_token
 
